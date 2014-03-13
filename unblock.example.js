e3134ddf2404c567a34c72a24aeafa4f36c28ade
Mac Heller-Ogden <mheller-ogden@cars.com>
2014-03-13 10:45:01 -0500

    initial commit

---
+++ unblock.example.js
@@ -0,0 +1,65 @@
+/**
+ * unblock - a single function for dead simple asynchronous control flow
+ *
+ * @author Mac Heller-Ogden
+ *
+ */
+
+var unblock = function (func) {
+    var result = [],
+        args = [],
+        queue = [],
+        fn = {
+        unblock: function (func) {
+            var a = args.slice(0);
+            return function () {
+                if (a.length < 1) a = (arguments.length) ? arguments : result;
+                setTimeout(function () {
+                    result = [func.apply(fn, a)];
+                    if (queue.length) queue.shift().apply(fn, result);
+                }, 0);
+                return fn;
+            };
+        },
+        then: function (func) {
+            queue.push(fn.unblock(func));
+            return fn;
+        },
+        after: function (func) {
+            return function () {
+                args = Array.prototype.slice.call(arguments, 0);
+                queue.push(fn.unblock(func));
+                return fn;
+            }
+        }
+    };
+    return fn.unblock(func);
+};
+
+
+/**
+ * example
+ */
+
+var sleep, report;
+
+// blocking function
+sleep = function (seconds) {
+    var ms, startTime;
+    ms = seconds * 1000;
+    startTime = new Date().getTime(); // get the current time
+    while (new Date().getTime() < startTime + ms); // hog cpu
+    return seconds;
+};
+
+// simple reporter
+report = function(x) { console.log(x); };
+
+// basic control flow example (view results in console)
+unblock(sleep)(2).then(report).after(sleep)(3).then(report);
+
+// since the above is unblocked, this will run first
+report(1);
+
+
+
---
+++ unblock.js
@@ -0,0 +1,37 @@
+/**
+ * unblock - a single function for dead simple asynchronous control flow
+ *
+ * @author Mac Heller-Ogden
+ *
+ */
+
+var unblock = function (func) {
+    var result = [],
+        args = [],
+        queue = [],
+        fn = {
+        unblock: function (func) {
+            var a = args.slice(0);
+            return function () {
+                if (a.length < 1) a = (arguments.length) ? arguments : result;
+                setTimeout(function () {
+                    result = [func.apply(fn, a)];
+                    if (queue.length) queue.shift().apply(fn, result);
+                }, 0);
+                return fn;
+            };
+        },
+        then: function (func) {
+            queue.push(fn.unblock(func));
+            return fn;
+        },
+        after: function (func) {
+            return function () {
+                args = Array.prototype.slice.call(arguments, 0);
+                queue.push(fn.unblock(func));
+                return fn;
+            }
+        }
+    };
+    return fn.unblock(func);
+};
